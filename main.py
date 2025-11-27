460ae6c9c311ff52556abf4aaedc1cc35aff93a2
Ibrahim <156953795+20mi17@users.noreply.github.com>
2025-11-27 12:52:06 -0500

    routers and health endpoints

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routers.health import router as health_router
 
 app = FastAPI()
+
+app.include_router(health_router)
 
 @app.get("/health")
 async def health_check():
